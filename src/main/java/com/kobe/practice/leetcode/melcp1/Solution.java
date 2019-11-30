a947a6cbf7e48d5089411449df5859d1a9f56198
hutao <15622719535@163.com>
2019-11-30 09:19:58 +0800

    practice

--- src/main/java/com/kobe/practice/leetcode/melcp1/Solution.java
+++ src/main/java/com/kobe/practice/leetcode/melcp1/Solution.java
@@ -1,10 +1,27 @@
 package com.kobe.practice.leetcode.melcp1;
 
 /**
- * @ClassName: Solution
- * @Description: TODO
+ * @ClassName: melcp1
+ * @Description: 猜数字
+ *
+ *  小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个，小A 每次也从 1, 2, 3 中选择一个猜。
+ *  他们一共进行三次这个游戏，请返回 小A 猜对了几次？
+ *
+ *  输入的guess数组为 小A 每次的猜测，answer数组为 小B 每次的选择。guess和answer的长度都等于3。
+ *
  * @Author: Tao
- * @Date 2019-11-30 09:16
+ * @Date 2019-11-30 09:07
  */
 public class Solution {
+
+    public int game(int[] guess, int[] answer) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (guess[i] == answer[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
